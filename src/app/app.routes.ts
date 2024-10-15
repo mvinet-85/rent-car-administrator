ffa29b073b54003032b5c73149eaba5a5382dba2
mvinet <vinetmanon85@gmail.com>
2024-10-15 23:18:08 +0200

    Création des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,35 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'register',
+    loadComponent: () => import('./register/register.page').then(m => m.RegisterPage)
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    redirectTo: 'register',
+    pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'car',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./car-list/car-list.page').then(m => m.CarListPage)
+      },
+      {
+        path: 'new',
+        loadComponent: () => import('./car-creation/car-creation.page').then(m => m.CarCreationPage)
+      },
+      {
+        path: ':id',
+        loadComponent: () => import('./car-detail/car-detail.page').then(m => m.CarDetailPage)
+      }
+    ]
+
+  }
 ];
