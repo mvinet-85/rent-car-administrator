7584df6dc30abb23a3445652d83b0e380ee75c11
mvinet <vinetmanon85@gmail.com>
2024-10-15 19:36:58 +0200

    Initialisation du projet

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rent-Car-Administrator',
+  webDir: 'www'
+};
+
+export default config;
