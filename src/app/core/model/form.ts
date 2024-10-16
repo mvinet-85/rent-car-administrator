92dd8bb2ba4f2f8846971590f3fd13bafadbffca
mvinet <vinetmanon85@gmail.com>
2024-10-16 22:38:47 +0200

    Ajout du form model

---
+++ src/app/core/model/form.ts
@@ -0,0 +1,8 @@
+import {FormControl} from "@angular/forms";
+
+export interface RegisterFormModel {
+  userName: FormControl<string | null>,
+  email: FormControl<string | null>,
+  phone: FormControl<string | null>,
+  password: FormControl<string | null>,
+}
