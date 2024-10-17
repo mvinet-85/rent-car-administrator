e37fd2f8a2ae87850f8450fa4ddc0c75d3d9cdde
mvinet <vinetmanon85@gmail.com>
2024-10-17 23:32:36 +0200

    Systeme d'authentification

---
+++ src/app/core/service/authentication/authentication.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {createUserWithEmailAndPassword, getAuth, UserCredential} from "firebase/auth";
+import {getDatabase, ref, set} from 'firebase/database';
+import {User} from "../../model/user";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() {
+  }
+
+  public signUpWithEmailAndPassword(user: User): Promise<boolean | unknown> {
+    console.log('user : ', user);
+    console.log('getDatabase() : ', getDatabase());
+    const database = getDatabase();
+
+    return new Promise((resolve, reject) => {
+      console.log('promise');
+      createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+        .then((userCreated: UserCredential) => {
+
+          set(ref(database, 'users/' + userCreated.user.uid), {
+            email: user.email,
+            userName: user.userName,
+            phone: user.phone,
+            password: user.password,
+          }).then(() => resolve(true))
+            .catch((error) => {
+              console.error(error);
+              reject(false)
+            });
+
+        }).catch((error) => {
+        console.error(error);
+        reject(error)
+      });
+    });
+  }
+}
