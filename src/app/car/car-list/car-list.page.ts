d86918611e45e1a3e60a2341673d334b909b2a22
mvinet <vinetmanon85@gmail.com>
2024-10-21 15:39:30 +0200

    Ajout bouton de redirection vers la page d'ajout d'une voiture

--- src/app/car/car-list/car-list.page.ts
+++ src/app/car/car-list/car-list.page.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {IonButton, IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.page.html',
   styleUrls: ['./car-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
 })
 export class CarListPage implements OnInit {
+
+  private readonly router: Router = inject(Router);
 
   constructor() {
   }
@@ -19,4 +22,7 @@ export class CarListPage implements OnInit {
     console.log("CarListPage");
   }
 
+  onNavigateToCreationPage() {
+    this.router.navigate(['car/new']);
+  }
 }
