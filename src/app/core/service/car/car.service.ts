6b7564a2e1ccab527843c1bc47be426508bb2c54
mvinet <vinetmanon85@gmail.com>
2024-10-23 22:25:08 +0200

    Ajout du getAllCar

--- src/app/core/service/car/car.service.ts
+++ src/app/core/service/car/car.service.ts
@@ -3,37 +3,53 @@ import {getDatabase, onValue, ref, set} from "firebase/database";
 import {Car} from "../../model/car";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CarService {
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  public saveCar(car: Car): Promise<void> {
-    return set(ref(getDatabase(), 'cars/' + car.licensePlate), {
-      brand: car.brand,
-      model: car.model,
-      licensePlate: car.licensePlate,
-      frontPicture: car.frontPicture,
-      behindPicture: car.behindPicture,
-    });
-  }
+    public saveCar(car: Car): Promise<void> {
+        return set(ref(getDatabase(), 'cars/' + car.licensePlate), {
+            brand: car.brand,
+            model: car.model,
+            licensePlate: car.licensePlate,
+            frontPicture: car.frontPicture,
+            behindPicture: car.behindPicture,
+        });
+    }
 
-  public getAllLicensePlate(): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-      const carsRef = ref(getDatabase(), 'cars/');
-      onValue(carsRef, (snapshot) => {
-        const data = snapshot.val();
-        const licensePlateArray: string[] = [];
-        if (data) {
-          Object.entries(data).forEach(value => {
-            const car: Car = value[1] as Car;
-            licensePlateArray.push(car.licensePlate);
-          });
-        }
-        resolve(licensePlateArray);
-      });
-    });
-  }
+    public getAllLicensePlate(): Promise<string[]> {
+        return new Promise((resolve, reject) => {
+            const carsRef = ref(getDatabase(), 'cars/');
+            onValue(carsRef, (snapshot) => {
+                const data = snapshot.val();
+                const licensePlateArray: string[] = [];
+                if (data) {
+                    Object.entries(data).forEach(value => {
+                        const car: Car = value[1] as Car;
+                        licensePlateArray.push(car.licensePlate);
+                    });
+                }
+                resolve(licensePlateArray);
+            });
+        });
+    }
+
+    public getAllCar(): Promise<Car[]> {
+        return new Promise((resolve, reject) => {
+            const contactsRef = ref(getDatabase(), 'cars/');
+            onValue(contactsRef, (snapshot) => {
+                const data = snapshot.val();
+                const contacts: Car[] = [];
+                if (data) {
+                    Object.entries(data).forEach(value => {
+                        contacts.push(value[1] as Car);
+                    });
+                }
+                resolve(contacts);
+            });
+        });
+    }
 }
