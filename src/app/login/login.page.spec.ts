47685480a24e2e979b51bf010eb43511a6d9953f
mvinet <vinetmanon85@gmail.com>
2024-10-23 21:46:20 +0200

    pb firebase pipeline

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,26 +1,24 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import {LoginPage} from './login.page';
 import {ActivatedRoute} from '@angular/router';
 import {of} from 'rxjs';
 
 describe('LoginPage', () => {
   let component: LoginPage;
-  let fixture: ComponentFixture<LoginPage>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginPage],
+      imports: [LoginPage],
       providers: [
         {
           provide: ActivatedRoute,
-          useValue: {params: of({id: 'test-id'})} // Vous pouvez fournir des valeurs simulées ici
+          useValue: {params: of({id: 'test-id'})}
         }
       ]
     }).compileComponents();
 
     const fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
--- src/app/register/register.page.spec.ts
+++ src/app/register/register.page.spec.ts
@@ -1,15 +1,14 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 import {RegisterPage} from './register.page';
 import {ActivatedRoute} from '@angular/router';
 import {of} from 'rxjs';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
-  let fixture: ComponentFixture<RegisterPage>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RegisterPage],
+      imports: [RegisterPage],
       providers: [
         {
           provide: ActivatedRoute,
@@ -20,7 +19,6 @@ describe('RegisterPage', () => {
 
     const fixture = TestBed.createComponent(RegisterPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
