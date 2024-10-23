f9d07ac648e083b184f54e0c1c5282acce3109bd
mvinet <vinetmanon85@gmail.com>
2024-10-23 22:26:07 +0200

    Page de liste des voitures

--- src/app/car/car-list/car-list.page.ts
+++ src/app/car/car-list/car-list.page.ts
@@ -1,25 +1,48 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {IonButton, IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 import {Router} from "@angular/router";
+import {Car} from "../../core/model/car";
+import {CarService} from "../../core/service/car/car.service";
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.page.html',
   styleUrls: ['./car-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonList, IonItem, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg]
 })
 export class CarListPage implements OnInit {
 
+  public cars: Car[] = [];
+
   private readonly router: Router = inject(Router);
+  private readonly carService: CarService = inject(CarService);
 
   constructor() {
   }
 
   ngOnInit() {
-    console.log("CarListPage");
+    this.carService.getAllCar()
+      .then((cars: Car[]) => {
+        this.cars = cars;
+      });
   }
 
   onNavigateToCreationPage() {
