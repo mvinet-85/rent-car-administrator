43a62f2da6246e404c4db6ad9726c9d671dddace
mvinet <vinetmanon85@gmail.com>
2024-10-23 21:12:40 +0200

    pb firebase pipeline

--- src/app/core/service/authentication/authentification.service.spec.ts
+++ src/app/core/service/authentication/authentification.service.spec.ts
@@ -1,11 +1,24 @@
 import {TestBed} from '@angular/core/testing';
 
 import {AuthenticationService} from './authentication.service';
+import {initializeApp} from "firebase/app";
 
 describe('AuthentificationService', () => {
   let service: AuthenticationService;
 
   beforeEach(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAtTadiTbfsVqRAK1fxolhABxc4gAJHKAI',
+      authDomain: 'rent-car-administrator.firebaseapp.com',
+      projectId: 'rent-car-administrator',
+      databaseURL: 'https://rent-car-administrator-default-rtdb.europe-west1.firebasedatabase.app/',
+      storageBucket: 'rent-car-administrator.appspot.com',
+      messagingSenderId: '943999344997',
+      appId: '1:943999344997:web:15ff3ff6f0dc0e5a9a018c'
+    };
+
+    initializeApp(firebaseConfig);
+    
     TestBed.configureTestingModule({});
     service = TestBed.inject(AuthenticationService);
   });
