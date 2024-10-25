981e5d44e3b2c2ee1fb38e85da50137597a24931
mvinet <vinetmanon85@gmail.com>
2024-10-25 15:08:19 +0200

    ajout mock sur les tests

--- src/app/car/car-detail/car-detail.page.spec.ts
+++ src/app/car/car-detail/car-detail.page.spec.ts
@@ -1,11 +1,30 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CarDetailPage } from './car-detail.page';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {CarDetailPage} from './car-detail.page';
+import {ActivatedRoute} from "@angular/router";
+import {of} from "rxjs";
 
 describe('CarDetailPage', () => {
   let component: CarDetailPage;
   let fixture: ComponentFixture<CarDetailPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CarDetailPage],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: {
+                get: () => 'some-id',
+              },
+            },
+            queryParams: of({someParam: 'someValue'}),
+          },
+        },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CarDetailPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
