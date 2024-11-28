80c0a5199caf016b6d12511185cd6b1cdbc300ca
mvinet <vinetmanon85@gmail.com>
2024-11-28 20:19:23 +0100

    Generation android/ios

---
+++ android/app/src/main/java/edu/rentCarAdministrator/app/MainActivity.java
@@ -0,0 +1,5 @@
+package edu.rentCarAdministrator.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
-import type {CapacitorConfig} from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'edu.rentCarAdministrator.app',
   appName: 'rent-car-administrator',
   webDir: 'www'
 };
